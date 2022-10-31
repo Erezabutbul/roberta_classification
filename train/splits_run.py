d0f3b8cec526bc421ad734880c64fb11b88f32b4
Erezabutbul <textours@gmail.com>
2022-10-31 10:17:49 +0200

    after fixing splits_run.py dir problem

--- train/splits_run.py
+++ train/splits_run.py
@@ -5,7 +5,7 @@ from sklearn.model_selection import StratifiedShuffleSplit, StratifiedKFold
 run_dir = os.path.realpath(os.path.join(os.path.dirname(__file__), '..'))
 
 res_df = pd.read_csv(
-    os.path.join(run_dir, 'data_full.csv'),
+    os.path.join(run_dir, 'train\\data\\data_full.csv'),
     on_bad_lines='skip', index_col=0)
 
 df = pd.DataFrame()
@@ -15,11 +15,11 @@ for train_index, val_index in skf.split(res_df["text"], res_df["labels"]):
     train_df = res_df.iloc[train_index]
     test_df = res_df.iloc[val_index]
     train_df.to_csv(
-        os.path.join(run_dir, 'train_balanced.csv'),
+        os.path.join(run_dir, 'train\\data\\train_balanced.csv'),
         index=True, index_label="index", encoding="utf_8_sig")
     df = train_df
     test_df.to_csv(
-        os.path.join(run_dir, 'test_balanced.csv'),
+        os.path.join(run_dir, 'train\\data\\test_balanced.csv'),
         index=True, index_label="index", encoding="utf_8_sig")
     break
 
@@ -33,6 +33,6 @@ for train_index, val_index in skf.split(df["text"], df["labels"]):
     train_str = "train_df_" + str(ctr) + ".csv"
     eval_str = "eval_df_" + str(ctr) + ".csv"
     train_df_tmp.to_csv(
-        os.path.join(run_dir, 'splits', train_str), index=True, index_label="index", encoding="utf_8_sig")
+        os.path.join(run_dir, 'train\\data\\splits', train_str), index=True, index_label="index", encoding="utf_8_sig")
     eval_df_tmp.to_csv(
-        os.path.join(run_dir, 'splits', eval_str), index=True, index_label="index", encoding="utf_8_sig")
+        os.path.join(run_dir, 'train\\data\\splits', eval_str), index=True, index_label="index", encoding="utf_8_sig")
